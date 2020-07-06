19c92e2be69b192f580f82c0ff392c925704d333
shibd <wudixiaolongbao@gmail.com>
2020-07-06 13:41:59 +0800

    feat: init

---
+++ src/main/java/com/baozi/grpc/Test.java
@@ -0,0 +1,12 @@
+package com.baozi.grpc;
+
+/**
+ * @author baozi
+ * @date 2020/7/5 9:43 PM
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
